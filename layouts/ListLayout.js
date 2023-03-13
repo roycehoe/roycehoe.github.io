1749efc03bf714fe8c5d0440a1d67ba9f9f8f218
roycehoe <royceinbox@hotmail.com>
2023-03-13 12:02:32 +0800

    Add truncation back to posts, but limit amount of truncation

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -51,33 +51,31 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
           {displayPosts.map((frontMatter) => {
             const { slug, date, title, summary, tags } = frontMatter
             return (
-              <li key={slug} className="py-4 ">
-                <article className="">
-                  <div className="article__summary space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <div className="space-y-3 xl:col-span-3">
-                      <div>
-                        <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                          <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                            {title}
-                          </Link>
-                        </h3>
-                        <div className="flex flex-wrap">
-                          {tags.map((tag) => (
-                            <Tag key={tag} text={tag} />
-                          ))}
-                        </div>
+              <li key={slug} className="py-4">
+                <article className="space-y-2 xl:grid xl:grid-cols-6 xl:items-baseline xl:space-y-0">
+                  <div className="space-y-3 xl:col-span-5">
+                    <div>
+                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                          {title}
+                        </Link>
+                      </h3>
+                      <div className="flex flex-wrap">
+                        {tags.map((tag) => (
+                          <Tag key={tag} text={tag} />
+                        ))}
                       </div>
                     </div>
-                    <dl className="">
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-right text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date)}</time>
-                      </dd>
-                    </dl>
+                    <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                      {summary}
+                    </div>
                   </div>
-                  <div className="article__header prose max-w-none text-gray-500 dark:text-gray-400 ">
-                    {summary}
-                  </div>
+                  <dl className="">
+                    <dt className="sr-only">Published on</dt>
+                    <dd className="text-right text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                      <time dateTime={date}>{formatDate(date)}</time>
+                    </dd>
+                  </dl>
                 </article>
               </li>
             )
