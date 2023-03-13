a8c0ca57d911c148700f4ad6b08ab30ca1f5740d
roycehoe <royceinbox@hotmail.com>
2023-03-13 11:51:26 +0800

    Shift date to align right

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -53,12 +53,6 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
             return (
               <li key={slug} className="py-4">
                 <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                  <dl>
-                    <dt className="sr-only">Published on</dt>
-                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                      <time dateTime={date}>{formatDate(date)}</time>
-                    </dd>
-                  </dl>
                   <div className="space-y-3 xl:col-span-3">
                     <div>
                       <h3 className="text-2xl font-bold leading-8 tracking-tight">
@@ -76,6 +70,12 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                       {summary}
                     </div>
                   </div>
+                  <dl className="">
+                    <dt className="sr-only">Published on</dt>
+                    <dd className="text-right text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                      <time dateTime={date}>{formatDate(date)}</time>
+                    </dd>
+                  </dl>
                 </article>
               </li>
             )
