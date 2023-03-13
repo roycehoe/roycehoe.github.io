412fca9724e4a2ba6a4aae01a17fc37467311ec4
roycehoe <royceinbox@hotmail.com>
2023-03-13 12:05:17 +0800

    Replace template site metadata with personal data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'FancyBinary',
+  author: 'Royce Hoe',
   headerTitle: 'FancyBinary',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A blog recounting my journey as a software engineer',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -10,12 +10,12 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'royceinbox@hotmail.com',
+  github: 'https://github.com/roycehoe/',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/findroycehere/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
