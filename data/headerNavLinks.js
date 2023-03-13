dbc53d721c106efc624eda22014005c5dc74cd06
roycehoe <royceinbox@hotmail.com>
2023-03-13 11:37:03 +0800

    Remove all posts header in home page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,6 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
 ]
 
