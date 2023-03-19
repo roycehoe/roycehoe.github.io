102cd2fd354cce662c284703f918de64893dcb98
roycehoe <royceinbox@hotmail.com>
2023-03-19 17:24:28 +0800

    feat: remove placeholder content from projects

--- pages/projects.js
+++ pages/projects.js
@@ -13,6 +13,7 @@ export default function Projects() {
             Projects
           </h1>
         </div>
+        <p className="pt-6"> Check out my github. </p>
         {/* <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
             {projectsData.map((d) => (
